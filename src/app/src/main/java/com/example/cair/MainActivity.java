62b0fe7313c63e9297d551915f3f6cb97bfd84da
yanchen01 <ychen01@bu.edu>
2020-02-11 19:15:24 -0500

    linked login and register btn

--- src/app/src/main/java/com/example/cair/MainActivity.java
+++ src/app/src/main/java/com/example/cair/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.cair;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button registerBtn = findViewById(R.id.register_btn);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(getApplicationContext(), Register.class);
+                // registerIntent.putExtra("key", value); //Optional parameters
+                startActivity(registerIntent);
+            }
+        });
+
+        final Button loginBtn = findViewById(R.id.login_btn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(getApplicationContext(), Login.class);
+                // loginIntent.putExtra("key", value); //Optional parameters
+                startActivity(loginIntent);
+            }
+        });
     }
 }
